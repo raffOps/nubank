06e4c8b039e174314d110abe5f9c11613a65eb52
Rafael <rjribeiro@inf.ufrgs.br>
2023-05-26 17:15:54 -0300

    checkpoint

--- queries/problem_statement_1.sql
+++ queries/problem_statement_1.sql
@@ -1,75 +1,83 @@
 with transfer_ins_transformed as (
-    select
-        id,
-        account_id,
-        amount,
-        to_timestamp(transaction_requested_at),
-        to_timestamp(transaction_completed_at),
-        status,
-        'NON PIX' as transaction_type,
-        'IN' as in_or_out
-    from transfer_ins
-),
-
-    transfer_outs_transformed as (
-    select
-        id,
-        account_id,
-        amount,
-        to_timestamp(transaction_requested_at),
-        to_timestamp(transaction_completed_at),
-        status,
-        'NON PIX' as transaction_type,
-        'OUT' as in_or_out
-    from transfer_outs
-),
-
-    pix_transformed as (
         select
             id,
             account_id,
-            pix_amount as amount,
+            amount,
+            to_timestamp(transaction_requested_at) as transaction_requested_at,
+            to_timestamp(transaction_completed_at) as transaction_completed_at,
+            status,
+            'NON PIX'                              as transaction_type,
+            'IN'                                   as in_or_out
+        from transfer_ins
+    ),
+
+     transfer_outs_transformed as (
+        select
+            id,
+            account_id,
+            amount,
+            to_timestamp(transaction_requested_at) as transaction_requested_at,
+            to_timestamp(transaction_completed_at) as transaction_completed_at,
+            status,
+            'NON PIX'                              as transaction_type,
+            'OUT'                                  as in_or_out
+          from transfer_outs
+    ),
+
+     pix_transformed as (
+        select
+            id,
+            account_id,
+            pix_amount                     as amount,
             to_timestamp(pix_requested_at) as transaction_requested_at,
             to_timestamp(pix_completed_at) as transaction_completed_at,
             status,
-            'PIX' as transaction_type,
+            'PIX'                          as transaction_type,
             case
                 when in_or_out = 'pix_out' then 'OUT'
                 when in_or_out = 'pix_in' then 'IN'
-            end as in_or_out
-        from pix_movements
+            end                        as in_or_out
+          from pix_movements
     ),
 
-    investments_transformed as (
+     investments_transformed as (
         select
-            transaction_id as id,
+            transaction_id                        as id,
             account_id,
             amount,
             to_timestamp(investment_requested_at) as transaction_requested_at,
             to_timestamp(investment_completed_at) as transaction_completed_at,
             status,
-            'NON PIX' as transaction_type,
+            'NON PIX'                             as transaction_type,
             case
                 when type = 'investment_transfer_out' then 'OUT'
                 when type = 'investment_transfer_in' then 'IN'
-            end as in_or_out
-        from investments
+            end                               as in_or_out
+          from investments),
+
+     transactions as (
+        select * from transfer_ins_transformed
+        union all
+        select * from transfer_outs_transformed
+        union all
+        select * from pix_transformed
+        union all
+        select * from investments_transformed
     ),
-    transactions as (select *
-                     from transfer_ins_transformed
-                     union all
-                     select *
-                     from transfer_outs_transformed
-                     union all
-                     select *
-                     from pix_transformed
-                     union all
-                     select *
-                     from investments_transformed
-)
+
+    montly_balance_temp as (
+        select
+            account_id,
+            cast(date_trunc('month', transaction_completed_at) as date) as year_month,
+            coalesce(sum(amount) filter (where in_or_out = 'IN' ), 0)   as total_transactions_in,
+            coalesce(sum(amount) filter (where in_or_out = 'OUT' ), 0)  as total_transactions_out
+        from transactions
+        where transaction_completed_at >= '2020-01-01'
+          and transaction_completed_at <= '2020-12-31'
+        group by account_id, date_trunc('month', transaction_completed_at)
+    )
 
 select
-    in_or_out,
-    count(*)
-from transactions
-group by in_or_out;
+    *,
+    total_transactions_in - total_transactions_out as account_monthly_balance
+from montly_balance_temp;
